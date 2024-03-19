13c4a9ef0ea1869c90bc2edb7855b044c0f5e2c8
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2024-03-19 14:46:01 +0700

    Complete feature review to the field type

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@
 
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :field
+  belongs_to :field_type
   has_many :comments
+
+  scope :reviews_newest_by, ->(field_type_ids) { where(field_type_id: field_type_ids).order(created_at: :desc) }
 end
