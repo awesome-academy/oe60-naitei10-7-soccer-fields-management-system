4d837e63f9cb5486642dafa8d856266d76f9e5be
Manh <manhddt2508@gmail.com>
2024-03-19 11:48:44 +0700

    Complete update, delete field types

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_13_083508) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_18_053706) do
   create_table "bookings", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "start_time"
     t.string "end_time"
