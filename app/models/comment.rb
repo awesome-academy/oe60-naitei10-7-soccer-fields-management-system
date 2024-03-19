54668b140c5d58c6ab178259b69a731985361c80
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2024-03-19 14:35:05 +0700

    Complete feature comment to the review

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :review
-  belongs_to :parent_comment
+  belongs_to :parent_comment, class_name: "Comment", optional: true
+
+  has_many :replies, class_name: "Comment", foreign_key: "parent_comment_id", dependent: :destroy
+
+  delegate :first_name, to: :user
 end
